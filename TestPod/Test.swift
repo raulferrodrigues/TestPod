33ef181da5e885d75f8209f77d19d3d2df22e2af
Raul <raulferrodrigues@gmail.com>
2019-07-23 17:01:15 -0300

    first

---
+++ TestPod/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  TestPod
+//
+//  Created by Raul Rodrigues on 7/19/19.
+//  Copyright © 2019 Raul Rodrigues. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    public static let helloWorld = "Hello world!"
+}
